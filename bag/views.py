c1f9b69a0ee1354785c13703bcce386be06023b8
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2020-11-08 14:16:42 +0000

    Update urls and views in bag and wishlist to be able to view product detail page for each item. Add view product button to wishlist

--- bag/views.py
+++ bag/views.py
@@ -110,3 +110,15 @@ def remove_from_bag(request, item_id):
     except Exception as e:
         messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
+
+
+def product_detail(request, product_id):
+    """ A view to show individual product details """
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/product_detail.html', context)
