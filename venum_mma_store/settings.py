9a951da9ff42b1a99abf5b4b6c15f8a6809ddd39
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2020-11-09 15:31:44 +0000

    Correct allowed hosts

--- venum_mma_store/settings.py
+++ venum_mma_store/settings.py
@@ -21,12 +21,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'idtjzdqh$8g^&pnmh!yg)m)twne7wao)83)e-x2gz&bgd18kx3'
+SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['venum=mma-store.herokuapp.com', 'localhost']
+ALLOWED_HOSTS = ['venum-mma-store.herokuapp.com', 'localhost']
 
 
 # Application definition
