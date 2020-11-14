702c2cf61d8de46f1af3b2c3f3693cf9a449b073
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2020-11-14 09:22:36 +0000

    Corrections to contact views

--- contact/views.py
+++ contact/views.py
@@ -2,6 +2,9 @@ from django.core.mail import send_mail, BadHeaderError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
 from .forms import ContactForm
+import os
+
+ADMINS_EMAIL = os.environ.get('ADMINS_EMAIL')
 
 
 def contactView(request):
@@ -15,11 +18,11 @@ def contactView(request):
             message = form.cleaned_data['message']
             try:
                 send_mail(subject, message, from_email,
-                          ["nikolasdrosatos@gmail.com"])
+                          [ADMINS_EMAIL])
             except BadHeaderError:
                 return HttpResponse('Invalid header found.')
             return redirect('success')
-    return render(request, "contact.html", {'form': form})
+    return render(request, 'contact', {'form': form})
 
 
 def successView(request):
