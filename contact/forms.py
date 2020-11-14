b4851605ce6b56e8c5d0edb4856c0121342f4b02
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2020-11-14 09:07:17 +0000

    Update contact app

--- contact/forms.py
+++ contact/forms.py
@@ -1,60 +1,7 @@
 from django import forms
-from .models import Contact
 
 
-class ContactForm(forms.ModelForm):
-    """
-    Form to allow the user to contact the site owners
-    """
-    class Meta:
-        model = Contact
-        fields = ['first_name',
-                  'last_name',
-                  'contact_subject',
-                  'email',
-                  'contact_body']
-
-    first_name = forms.CharField(
-        required=True,
-        label='First Name:',
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'First Name'
-        })
-    )
-
-    last_name = forms.CharField(
-        required=True,
-        label='Last Name:',
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Last Name'
-        })
-    )
-
-    contact_subject = forms.CharField(
-        required=True,
-        label='Subject:',
-        widget=forms.TextInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Subject'
-        })
-    )
-
-    email = forms.EmailField(
-        required=True,
-        label='Email:',
-        widget=forms.EmailInput(attrs={
-            'class': 'form-control',
-            'placeholder': 'Email'
-        })
-    )
-
-    contact_body = forms.CharField(
-        required=True,
-        label='Please enter your message:',
-        widget=forms.Textarea(attrs={
-            'class': 'form-control',
-            'placeholder': 'Message'
-        })
-    )
+class ContactForm(forms.Form):
+    from_email = forms.EmailField(required=True)
+    subject = forms.CharField(required=True)
+    message = forms.CharField(widget=forms.Textarea, required=True)
--- contact/views.py
+++ contact/views.py
@@ -1,77 +1,26 @@
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render, redirect
-from django.contrib import messages
-from django.conf import settings
-
-from django.core.mail import send_mail
-import os
 from .forms import ContactForm
-from .models import Contact
 
 
-"""Created an env var for the admin email so not in code. """
-ADMINS_EMAIL = os.environ.get('ADMINS_EMAIL')
-
-
-def contact(request):
-    """
-    Create the contact view. Check if the user is authenticated, pass
-    values to the contact form based on it.
-    When Post, An email should then be sent to the admins.
-    If not post pass through a blank version of the form.
-    """
-    if request.method == 'POST':
-        if request.user.is_authenticated:
-
-            form = Contact(
-                first_name=request.POST['first_name'],
-                last_name=request.POST['last_name'],
-                contact_subject=request.POST['contact_subject'],
-                email=request.POST['email'],
-                contact_body=request.POST['contact_body'],
-                query_user=request.user
-            )
-
-            form.save()
-
-        else:
-            form = Contact(
-                first_name=request.POST['first_name'],
-                last_name=request.POST['last_name'],
-                contact_subject=request.POST['contact_subject'],
-                email=request.POST['email'],
-                contact_body=request.POST['contact_body'],
-            )
-
-            form.save()
-
-        send_mail(
-            'Hello!',
-            'You have a new message. See admin panel for details.',
-            os.environ.get('SITE_EMAIL'),
-            [ADMINS_EMAIL],
-            fail_silently=False,
-        )
-
-        messages.success(request,
-                         'Thank you for contacting VENUM MMA STORE,'
-                         'our customer service will respond within the day.')
-        return redirect('contact')
-
+def contactView(request):
+    if request.method == 'GET':
+        form = ContactForm()
     else:
-        if request.user.is_authenticated:
-            form = ContactForm(
-                initial={
-                    'first_name': request.user.first_name,
-                    'last_name': request.user.last_name,
-                    'email': request.user.email
-                },
-            )
-        else:
-            form = ContactForm()
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            subject = form.cleaned_data['subject']
+            from_email = form.cleaned_data['from_email']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, from_email,
+                          ["nikolasdrosatos@gmail.com"])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('success')
+    return render(request, "contact.html", {'form': form})
 
-    context = {
-        'contact_page': 'active',
-        'form': form,
-    }
 
-    return render(request, 'contact/contact.html', context)
+def successView(request):
+    return HttpResponse('Thank you for contacting Venum MMA Store. We will get back to you shortly.')
