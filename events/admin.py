f2cc990589ff0d6c8d993da6172e3f6656e8ee65
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2020-11-18 14:07:40 +0000

    Finish events app

--- events/admin.py
+++ events/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
+from .models import Event
 
 # Register your models here.
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'day',
+        'start_time',
+        'location',
+        'image',
+    )
+
+    ordering = ('name',)
+
+
+admin.site.register(Event, EventAdmin)
