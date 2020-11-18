a1c02c27f43ffd94b334e275998a8f361d4c70af
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2020-11-18 13:15:05 +0000

    Add image and image url field to events.models, edit template, add images

--- events/models.py
+++ events/models.py
@@ -6,6 +6,8 @@ class Event(models.Model):
     day = models.DateField(u'Day of event', help_text=u'Day of the event')
     start_time = models.TimeField(u'Starting time', help_text=u'Starting time')
     location = models.CharField(max_length=200)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
 
     def __str__(self):
         return self.name
