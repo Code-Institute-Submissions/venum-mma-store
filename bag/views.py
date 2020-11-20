633d5f34a0912fe800e2703ac362932f3a1cd135
NikolasPolychronidis <nikolasdrosatos@gmail.com>
2020-11-20 16:46:40 +0000

    Add admin reg to profiles.admin

--- bag/views.py
+++ bag/views.py
@@ -1,6 +1,7 @@
 from products.models import Product
 from django.contrib import messages
-from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
+from django.shortcuts import (render, redirect, reverse,
+                              HttpResponse, get_object_or_404)
 
 
 # Create your views here.
